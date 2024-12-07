7becd43b835eff7a5bbca2dc5284b73eff9e94f0
jegu <worms773789@gmail.com>
2024-12-08 02:53:22 +0900

    Complete EC2_SET_UP.sh

---
+++ Project/EC2_SET_UP.sh
@@ -0,0 +1,32 @@
+echo '======== [1-1] 패키지 업데이트 ========'
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+echo '======== [1-2] 타임존 설정 ========'
+sudo timedatectl set-timezone Asia/Seoul
+
+echo '======== [1-3] Git 설치 ========'
+sudo apt install git-all
+
+
+echo '======== [2-1] Docker 설치 ========'
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+echo '======== [2-2] Docker 설치 ========'
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo '======== [2-3] Docker Compose 설치 ========'
+sudo apt-get update
+sudo apt-get install docker-compose-plugin
+
